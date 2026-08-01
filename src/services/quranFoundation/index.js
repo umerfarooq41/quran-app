79b2f2b4a4d4a481039555bb0dd202c984452d17
umerfarooq41 <umerchaudhary41@gmail.com>
2026-08-02 00:24:51 +0300

    Add files via upload

--- src/services/quranFoundation/index.js
+++ src/services/quranFoundation/index.js
@@ -1,8 +1,11 @@
 export { qfGet } from './client';
 export {
   WORD_BY_WORD_LANGUAGES,
+  WORD_BY_WORD_SOURCES,
   clearWordTranslationCache,
   getWordByWordTranslation,
+  getWordByWordSource,
+  getWordByWordSourcesForLanguage,
   getWordLanguage,
   normalizeWordLanguage,
 } from './words';
